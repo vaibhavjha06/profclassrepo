c6c1bf5844ea09590d4237c3e5d59ad2b890cdee
[Vaibhav Jha] <vaibhavjha010@gmail.com>
2024-02-23 21:05:46 -0500

    madlib game

---
+++ madlib.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+clear
+echo "Let's build a mad-lib!"
+
+read -p "1. Please give me an adjective: " ADJ1
+read -p "2. Please give me a sport: " SPO1
+read -p "3. Please give me a dessert: " DES1
+read -p "4. Please give me a university: " UNI1
+read -p "5. Please give me a country: " COU1
+read -p "6. Please give me a utensil: " UTE1
+read -p "7. Please give me a president: " PRE1
+read -p "8. Please give me a TV channel: " TVC1
+
+echo "It was a $ADJ1 day when $PRE1 was playing $SPO1 with his friends. At $UNI1, $PRE1 was feeling lethargic after eating the $DES1 at the dining hall. The $UTE1 was contaminated with bacteria imported from $COU1, which he hadn't realized until watching the news on $TVC1."
+
