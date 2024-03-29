c8508eebf62349ed35211ff4ebbdf7dcfbcbb1ee
[Vaibhav Jha] <vaibhavjha010@gmail.com>
2024-03-29 15:41:52 -0400

    Fixed gist file

--- mywork/lab5/labscript.sh
+++ mywork/lab5/labscript.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-/bin/curl -O https://gist.github.com/vaibhavjha06/1a27ff3b4068752f229a15608a839843.js
+/bin/curl -O https://gist.githubusercontent.com/vaibhavjha06/1a27ff3b4068752f229a15608a839843/raw/62aa340db0ba005dd4fe364d8f685ebd4ed0e65e/bootcore.sh
+/bin/bash bootstrap.sh
