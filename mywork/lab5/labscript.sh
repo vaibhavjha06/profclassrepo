1298dccaab5c5b839476a4771881d6d09141c326
[Vaibhav Jha] <vaibhavjha010@gmail.com>
2024-03-29 15:29:37 -0400

    edited link

--- mywork/lab5/labscript.sh
+++ mywork/lab5/labscript.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-/bin/curl -O https://github.com/vaibhavjha06/profclassrepo/blob/main/mywork/lab5/bootstrapscript.sh
+/bin/curl -O https://gist.github.com/vaibhavjha06/1a27ff3b4068752f229a15608a839843.js
+/bin/bash bootstrap.sh
